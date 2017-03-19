df982f2e0e6ccea1911d7d6c1c1f10d584393be7
Laser Roger <laserroger@live.com>
2017-03-20 03:10:00 +0800

    up

---
+++ upstream.py
@@ -0,0 +1,8 @@
+import os
+sudoPass = '04636331'
+command = 'cp -r output/* output_site/'
+os.system('echo %s|sudo -S %s' % (sudoPass, 'rm -rf output/'))
+os.system('echo %s|sudo -S %s' % (sudoPass, 'rm -rf output_site/*'))
+os.system('make publish')
+os.system('echo %s|sudo -S %s' % (sudoPass, command))
+os.system('cd output_site && git commit -a -m "new" && git push')
